04e0a1855968ac295315dabdd21d210ea6d3d944
jorgefacundo <jorge.facundo@encora.com>
2024-09-20 11:33:00 -0500

    first commit

--- frontend/src/components/MainLayout/Header.tsx
+++ frontend/src/components/MainLayout/Header.tsx
@@ -1,5 +1,12 @@
+import { GiFruitBowl } from "react-icons/gi";
+
 function Header() {
-  return <div className=" h-full"></div>;
+  return (
+    <div className="flex items-center p-4">
+      <GiFruitBowl className="text-3xl mr-4" />
+      <h1 className="text-3xl font-mono font-bold">Products Management</h1>
+    </div>
+  );
 }
 
 export default Header;
